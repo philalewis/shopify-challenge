2c15553ff37f1ada20b9910b5e854670e6c9a0b5
Phil Lewis <phillewis1985@gmail.com>
2022-05-10 14:17:56 -0600

    Set up network request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Form from './Form'
-
+import Response from './Response'
+import { sendPrompt } from './apiCalls'
 
 const App = () => {
   const [ responses, setResponses ] = useState([])
+
+  // useEffect(() => {
+  //   sendPrompt('Suggest one name for a horse.')
+  // }, [])
 
   return (
     <main>
