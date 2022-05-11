d089fd4d2f170905923845d5407d025fbc1de8fc
Phil Lewis <phillewis1985@gmail.com>
2022-05-11 11:11:46 -0600

    Write Readme

--- src/Response.js
+++ src/Response.js
@@ -3,14 +3,18 @@ import './Response.css'
 
 const Response = ({ responses }) => {
   const buildResponses = () => {
-    return responses.map((response, i) => {
-      return <article key={i} className="response-card">
-        <h4>Prompt: </h4>
-        <p className="prompt-value">{response.prompt}</p>
-        <h4>Response: </h4>
-        <p className="response-value">{response.response.trim()}</p>
-      </article>
-    })
+    if (responses.length > 0) {
+      return responses.map((response, i) => {
+        return <article key={i} className="response-card">
+          <h4>Prompt: </h4>
+          <p className="prompt-value">{response.prompt}</p>
+          <h4>Response: </h4>
+          <p className="response-value">{response.response.trim()}</p>
+        </article>
+      })
+    } else {
+      return <p>You have not submitted any prompts yet.</p>
+    }
   }
 
   return (
